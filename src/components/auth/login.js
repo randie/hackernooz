dc549037ba0f45622ed1475827006c5556b9f923
Randie B <randie.bemis@gmail.com>
2019-06-06 16:18:59 -0700

    create useFormValidation hook, use it in Login

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,15 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useFormValidation from '../../utils/use-form-validation';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 function Login(props) {
+  const { handleChange, handleSubmit, values } = useFormValidation(initialState);
   const [isLogin, setIsLogin] = React.useState(true);
 
   return (
     <div>
       <h2 className="mv3">{isLogin ? 'Login' : 'Create Account'}</h2>
-      <form className="flex flex-column">
-        {!isLogin && <input type="text" placeholder="Name" autoComplete="off" />}
-        <input type="email" placeholder="Email" autoComplete="off" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        {!isLogin && (
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            autoComplete="off"
+            value={values.name}
+            onChange={handleChange}
+          />
+        )}
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          autoComplete="off"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={values.password}
+          onChange={handleChange}
+        />
         <div className="flex mt3">
           <button type="submit" className="button pointer mr2">
             Submit
