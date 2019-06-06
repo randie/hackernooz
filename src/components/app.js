a232be8309ff7be98c6588832d3350f34305c43e
Randie B <randie.bemis@gmail.com>
2019-06-06 01:07:33 -0700

    stub out Header and Login components

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './auth/login';
+import Header from './header';
 
 function App() {
-  return <div>HackerNooz</div>;
+  return (
+    <Router>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.css';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 
