267c0219d2d167ef2d0e9284e4640aedb3c97a30
Randie B <randie.bemis@gmail.com>
2019-06-06 01:49:31 -0700

    style Header and add logo, title and login navlink

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="flex">
+        <img src="/logo.png" alt="Hacker Nooz Logo" className="logo" />
+        <NavLink to="/" className="header-title">
+          Hacker Newz
+        </NavLink>
+      </div>
+      <div className="flex">
+        <NavLink to="/login" className="header-link">
+          login
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default withRouter(Header);
