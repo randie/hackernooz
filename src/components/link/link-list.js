0f168ac5a64cadf693ca6ad644f654c19ccd6df7
Randie B <randie.bemis@gmail.com>
2019-06-11 00:11:40 -0700

    fetch links from firebase

--- src/components/link/link-list.js
+++ src/components/link/link-list.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { FirebaseContext } from '../../firebase';
 
 function LinkList() {
+  const { firebase } = React.useContext(FirebaseContext);
+
+  const getLinksCallback = React.useCallback(
+    () => firebase.db.collection('links').onSnapshot(handleSnapshot),
+    [firebase.db],
+  );
+
+  React.useEffect(() => {
+    getLinksCallback();
+  }, [getLinksCallback]);
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    console.log('>> links:', links);
+  }
+
   return <div>LinkList</div>;
 }
 
