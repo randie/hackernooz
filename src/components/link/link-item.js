4b1cbe63913862b188f01d9990bb3004656b62a5
Randie B <randie.bemis@gmail.com>
2019-06-11 17:12:17 -0700

    implement voting

--- src/components/link/link-item.js
+++ src/components/link/link-item.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import { FirebaseContext } from '../../firebase';
 
 const getDomain = url => url.replace(/^https?:\/\//i, '');
 
-function LinkItem({ link, index, showCount }) {
+function LinkItem({ link, index, showCount, history }) {
+  const { firebase, currentUser } = React.useContext(FirebaseContext);
+
+  async function handleVote() {
+    if (!currentUser) {
+      history.push('/login');
+      return;
+    }
+    const linkRef = firebase.db.collection('links').doc(link.id);
+    const doc = await linkRef.get();
+    if (doc.exists) {
+      const previousVotes = doc.data().votes;
+      const newVote = { votedBy: { id: currentUser.uid, name: currentUser.displayName } };
+      linkRef.update({ votes: [...previousVotes, newVote] });
+    }
+  }
+
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
         {showCount && <span className="gray">{index}.</span>}
-        <div className="vote-button">▲</div>
+        {/* TODO: hide vote button if currentUser is the link owner or has already voted before */}
+        <div className="vote-button" onClick={handleVote}>
+          ▲
+        </div>
       </div>
       <div className="ml1">
         <div>
@@ -27,4 +47,4 @@ function LinkItem({ link, index, showCount }) {
   );
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
